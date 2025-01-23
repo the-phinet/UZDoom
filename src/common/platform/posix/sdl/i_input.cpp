197ced73555d6b8653e360175a50a31502d82449
Gutawer <russj159@gmail.com>
2025-01-23 00:04:58 +0000

    improve SDL mouse handling in menus

--- src/common/platform/posix/sdl/i_input.cpp
+++ src/common/platform/posix/sdl/i_input.cpp
@@ -244,12 +244,12 @@ void I_SetMouseCapture()
 {
 	// Clear out any mouse movement.
 	SDL_GetRelativeMouseState (NULL, NULL);
-	SDL_SetRelativeMouseMode (SDL_TRUE);
+	SDL_CaptureMouse (SDL_TRUE);
 }
 
 void I_ReleaseMouseCapture()
 {
-	SDL_SetRelativeMouseMode (SDL_FALSE);
+	SDL_CaptureMouse (SDL_FALSE);
 }
 
 static void MouseRead ()
@@ -279,10 +279,12 @@ static void I_CheckNativeMouse ()
 	{
 		NativeMouse = wantNative;
 		SDL_ShowCursor (wantNative);
-		if (wantNative)
-			I_ReleaseMouseCapture ();
-		else
-			I_SetMouseCapture ();
+		if (wantNative) {
+			SDL_SetRelativeMouseMode (SDL_FALSE);
+		} else {
+			SDL_GetRelativeMouseState (NULL, NULL);
+			SDL_SetRelativeMouseMode (SDL_TRUE);
+		}
 	}
 }
 
