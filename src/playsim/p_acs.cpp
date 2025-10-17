ab5ec3c2d160ef6be7eefd80c6cfad558920568d
Moises Aguirre <aguirremoises@proton.me>
2025-10-16 20:40:13 -0400

    Fixed unnecessary runaway counter increase and uninitialized value.

--- src/playsim/p_acs.cpp
+++ src/playsim/p_acs.cpp
@@ -6905,7 +6905,7 @@ int DLevelScript::RunScript()
 	ACSLocalArrays noarrays;
 	ACSLocalArrays *localarrays = &noarrays;
 	ScriptFunction *activeFunction = NULL;
-	ScriptPtr *ptr;
+	ScriptPtr *ptr = nullptr;
 	FRemapTable *translation = 0;
 	int resultValue = 1;
 	int transi = -1;
@@ -6992,7 +6992,7 @@ int DLevelScript::RunScript()
 
 	while (state == SCRIPT_Running)
 	{
-		if ( (++runaway > 2000000) && ptr && !(ptr->Flags & SCRIPTF_Busy) )
+		if ( ptr && !(ptr->Flags & SCRIPTF_Busy) && (++runaway > 2000000) )
 		{
 			Printf ("Runaway %s terminated\n", ScriptPresentation(script).GetChars());
 			state = SCRIPT_PleaseRemove;
