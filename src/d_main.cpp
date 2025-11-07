3b0725f90c8a33697f765e61cc5a5149b761210c
Marcus Minhorst <marcus.minhorst@gmail.com>
2025-11-06 20:21:57 -0500

    Skin dir follows xdg stuff

--- src/d_main.cpp
+++ src/d_main.cpp
@@ -2070,7 +2070,8 @@ static void AddAutoloadFiles(const char *autoname, std::vector<std::string>& all
 		D_AddDirectory (allwads, file.GetChars(), "*.wad", GameConfig);
 
 #ifdef __unix__
-		file = NicePath("$HOME/" GAME_DIR "/skins");
+		FString skinDir = FStringf("%s/games/" GAMENAMELOWERCASE "/skins", GetDataPath());
+		file = NicePath(skinDir.GetChars());
 		D_AddDirectory (allwads, file.GetChars(), "*.wad", GameConfig);
 #endif	
 
