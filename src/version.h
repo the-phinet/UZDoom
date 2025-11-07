0253b27eb22205f935a94564a003ee5703b2f9fa
Marcus Minhorst <marcus.minhorst@gmail.com>
2025-11-06 20:26:53 -0500

    Removed unused constant

--- src/version.h
+++ src/version.h
@@ -119,8 +119,6 @@ const char *GetVersionString();
 #define GAME_DIR GAMENAME
 #elif defined(__HAIKU__)
 #define GAME_DIR "config/settings/" GAMENAME
-#else
-#define GAME_DIR ".config/" GAMENAMELOWERCASE
 #endif
 
 #define DEFAULT_DISCORD_APP_ID "1428620310302691349"
@@ -129,6 +127,5 @@ const int SAVEPICWIDTH = 216;
 const int SAVEPICHEIGHT = 162;
 const int VID_MIN_WIDTH = 320;
 const int VID_MIN_HEIGHT = 200;
-
 
 #endif //__VERSION_H__
