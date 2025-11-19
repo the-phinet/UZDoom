3b32e6b63ba6cbf326b57b5912e4a01d31d48573
Boondorl <boondorl@live.com>
2025-11-18 21:14:47 -0500

    rc2 version

--- src/version.h
+++ src/version.h
@@ -41,7 +41,7 @@ const char *GetVersionString();
 
 /** Lots of different version numbers **/
 
-#define VERSIONSTR "4.14.3-rc1"
+#define VERSIONSTR "4.14.3-rc2"
 
 // The version as seen in the Windows resource
 #define RC_FILEVERSION 4,14,3,0
