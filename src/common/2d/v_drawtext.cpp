c6f91568ca1c7bbf219b0efd1633dda607c0db27
River-Salmon <40964273+River-Salmon@users.noreply.github.com>
2025-12-07 19:45:06 -0800

    re-use the TextShaper for each font
    before we were constructing a new TextShaper for each text draw. Now we re-use the existing one. This is a considerable performance improvement when shaped text is on screen

--- src/common/2d/v_drawtext.cpp
+++ src/common/2d/v_drawtext.cpp
@@ -40,6 +40,7 @@
 #include <type_traits>
 #include <span>
 #include <string>
+#include "Trex/Atlas.hpp"
 
 int ListGetInt(VMVa_List &tags);
 
@@ -277,8 +278,8 @@ void DrawTextCommon(F2DDrawer *drawer, FFont *font, int normalcolor, double x, d
 
 	if (font && font->IsValidDynamicFont())
 	{
-		const auto&atlas = *font->GetDynamicFontAtlas();
-		auto               shaper = Trex::TextShaper(atlas);
+		const Trex::Atlas& atlas = *font->GetDynamicFontAtlas();
+		Trex::TextShaper& shaper = *font->GetDynamicTextShaper();
 		Trex::ShapedGlyphs glyphs;
 		if constexpr (std::is_same_v<chartype, char>)
 		{
