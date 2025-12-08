ee54f5c3ad0633ae8a7557b851c203225ce0de72
River-Salmon <40964273+River-Salmon@users.noreply.github.com>
2025-12-08 00:29:07 -0800

    directly override NewSmallFont when language changes
    This is potentially a temporary solution to JP and KO, in those cases we know that the primary font probably cannot display the necessary glyphs. In that case, we should directly switch over to the JP and KO fonts to get the correct text shaping in menus.

--- src/common/fonts/font.cpp
+++ src/common/fonts/font.cpp
@@ -596,6 +596,9 @@ void FFont::UpdateFontDynamicFallbacks(const char* lang)
 		}
 		font = font->Next;
 	}
+
+	//the primary small font should also be changed so correct text shaping is used in menus.
+	NewSmallFont = FindDynamicFallbackFontForLanguage(lang);
 }
 
 //==========================================================================
