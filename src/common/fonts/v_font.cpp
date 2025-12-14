ba01875da8b1a2ba8cd741af7e6c9b90dbb1dab5
River-Salmon <40964273+River-Salmon@users.noreply.github.com>
2025-12-13 23:44:39 -0800

    Properly grab fallback font at startup

--- src/common/fonts/v_font.cpp
+++ src/common/fonts/v_font.cpp
@@ -48,6 +48,7 @@
 #include "fontinternals.h"
 #include "fs_files.h"
 #include <span>
+#include "c_cvars.h"
 
 // MACROS ------------------------------------------------------------------
 
@@ -224,7 +225,8 @@ void V_InitCustomFonts()
 		for (auto *font : fontsWeAdded)
 		{
 			const FName  fontName        = font->GetName();
-			FFont *const dynamicFallback = V_GetFont("IBMPLEXS");
+			auto         cvar            = FindCVar("fontoverride_Fallback", nullptr);
+			FFont *const dynamicFallback = V_GetFont(cvar->GetHumanString());
 			font->SetDynamicFallback(dynamicFallback);
 		}
 	}
