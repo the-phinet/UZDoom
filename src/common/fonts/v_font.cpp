ffb52226e8ddfda17de444150961d73f51a675bd
River-Salmon <40964273+River-Salmon@users.noreply.github.com>
2025-12-21 23:03:12 -0800

    removed unused line

--- src/common/fonts/v_font.cpp
+++ src/common/fonts/v_font.cpp
@@ -934,7 +934,6 @@ void V_InitFonts()
 	}
 	
 	CurrentConsoleFont = NewConsoleFont;
-	//BigUpper           = V_GetFont("WDXLLUBR");
 	ConFont = V_GetFont("ConsoleFont", "CONFONT");
 	V_GetFont("IndexFont", "INDEXFON");	// detect potential replacements for this one.
 }
