b0ad5403fcb985cca27c55fa37d746bb44aacf32
River-Salmon <40964273+River-Salmon@users.noreply.github.com>
2026-01-16 16:52:32 -0800

    Chat input uses user font choice for Console

--- src/ct_chat.cpp
+++ src/ct_chat.cpp
@@ -253,7 +253,7 @@ void CT_Drawer (void)
 {
 	auto &vp = r_viewpoint;
 	auto drawer = twod;
-	FFont *displayfont = NewConsoleFont;
+	FFont *displayfont = FFont::GetConsoleFont(NewConsoleFont);
 
 	HU_DrawScores(vp.TicFrac);
 
@@ -290,6 +290,11 @@ void CT_Drawer (void)
 
 		promptwidth = displayfont->StringWidth (prompt) * scalex;
 		x = displayfont->GetCharWidth (displayfont->GetCursor()) * scalex * 2 + promptwidth;
+
+		if (displayfont->IsValidDynamicFont())
+		{
+			y -= displayfont->GetHeight();
+		}
 
 		FString printstr = ChatQueue;
 		// figure out if the text is wider than the screen
