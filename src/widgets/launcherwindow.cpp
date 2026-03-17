d995d35adb2d5d12286f18356665b74715fc164c
Ricardo Luís Vaz Silva <ricolvs123@gmail.com>
2026-03-17 03:33:42 -0300

    fix macos/linux compilation

--- src/widgets/launcherwindow.cpp
+++ src/widgets/launcherwindow.cpp
@@ -110,6 +110,13 @@ LauncherWindow::LauncherWindow(FStartupSelectionInfo& info) : Widget(nullptr, Wi
 #endif
 }
 
+#ifndef HAS_UPDATER
+void LauncherWindow::OnWindowClose()
+{
+	Close();
+}
+
+#endif
 void LauncherWindow::UpdatePlayButton()
 {
 	Buttonbar->UpdateLanguage();
