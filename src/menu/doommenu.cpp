c6d2c3e71cd42030874369167b180309ca7c5924
River-Salmon <40964273+River-Salmon@users.noreply.github.com>
2026-05-19 13:54:14 -0700

    no longer force MainMenuTextOnly when using dynamic fonts:
    -Fixes being unable to access the Codex/Manual in Reelism 2.
    -The main menu text items will still be substituted with dynamic text, so this is still functional. Mostly.

--- src/menu/doommenu.cpp
+++ src/menu/doommenu.cpp
@@ -167,7 +167,8 @@ bool M_SetSpecialMenu(FName& menu, int param)
 		if (gameinfo.gametype & GAME_DoomStrifeChex)	// Raven's games always used text based menus
 		{
 			FFont *desiredMenuFont = FFont::GetBigTextFont(BigUpper);
-			if (generic_ui || gameinfo.forcetextinmenus || (desiredMenuFont && desiredMenuFont->IsValidDynamicFont()))	// If text is forced, this overrides any check.
+			const bool isDynamicFont   = (desiredMenuFont && desiredMenuFont->IsValidDynamicFont());
+			if (generic_ui || gameinfo.forcetextinmenus)	// If text is forced, this overrides any check.
 			{
 				menu = NAME_MainMenuTextOnly;
 			}
