e1bddbc837540e429d045ff0fd6b8a56ba060484
Ricardo Luís Vaz Silva <ricolvs123@gmail.com>
2026-06-01 19:58:46 -0300

    Move rapidjson includes to its own header

--- src/common/engine/serializer.cpp
+++ src/common/engine/serializer.cpp
@@ -22,16 +22,7 @@
 **
 */
 
-#define RAPIDJSON_48BITPOINTER_OPTIMIZATION 0	// disable this insanity which is bound to make the code break over time.
-#define RAPIDJSON_HAS_CXX11_RVALUE_REFS 1
-#define RAPIDJSON_HAS_CXX11_RANGE_FOR 1
-#define RAPIDJSON_PARSE_DEFAULT_FLAGS kParseFullPrecisionFlag
-
-#include <miniz.h>
-#include "rapidjson/rapidjson.h"
-#include "rapidjson/writer.h"
-#include "rapidjson/prettywriter.h"
-#include "rapidjson/document.h"
+#include "serializer_rapidjson.h"
 #include "serializer.h"
 #include "dobject.h"
 #include "filesystem.h"
