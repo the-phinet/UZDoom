cf2d783abd34726c56e7cafc6402b770faaa5c40
Marcus Minhorst <marcus.minhorst@gmail.com>
2026-08-25 00:19:12 -0400

    Add image sizing options

--- libraries/ZWidget/include/zwidget/widgets/imagebox/imagebox.h
+++ libraries/ZWidget/include/zwidget/widgets/imagebox/imagebox.h
@@ -4,11 +4,36 @@
 #include "../../core/widget.h"
 #include "../../core/image.h"
 
-enum ImageBoxMode
+enum ImageBoxScale
 {
-	Center,
-	Contain,
-	Cover
+	None     = 0,
+	Contain  = 0b0'000'000'01,
+	Cover    = 0b0'000'000'10,
+
+	StretchX = 0b0'000'100'00,
+	GrowX    = 0b0'000'010'00,
+	ShrinkX  = 0b0'000'001'00,
+
+	StretchY = 0b0'100'000'00,
+	GrowY    = 0b0'010'000'00,
+	ShrinkY  = 0b0'001'000'00,
+
+	Stretch = StretchX|StretchY,
+	Grow    = GrowX|GrowY,
+	Shrink  = ShrinkX|ShrinkY,
+};
+
+enum ImageBoxAnchor
+{
+	Center = 0,
+	North  = 0b00010,
+	South  = 0b00011,
+	East   = 0b01000,
+	West   = 0b01100,
+	NorthEast = North|East,
+	NorthWest = North|West,
+	SouthEast = South|East,
+	SouthWest = South|West,
 };
 
 class ImageBox : public Widget
@@ -17,7 +42,8 @@ public:
 	ImageBox(Widget* parent);
 
 	void SetImage(std::shared_ptr<Image> newImage);
-	void SetImageMode(ImageBoxMode mode);
+	void SetImageScale(ImageBoxScale scale);
+	void SetImageAnchor(ImageBoxAnchor anchor);
 
 	double GetPreferredWidth() override;
 	double GetPreferredHeight() override;
@@ -27,5 +53,6 @@ protected:
 
 private:
 	std::shared_ptr<Image> image;
-	ImageBoxMode mode = ImageBoxMode::Center;
+	ImageBoxScale scale = ImageBoxScale::None;
+	ImageBoxAnchor anchor = ImageBoxAnchor::Center;
 };
--- libraries/ZWidget/src/widgets/imagebox/imagebox.cpp
+++ libraries/ZWidget/src/widgets/imagebox/imagebox.cpp
@@ -1,3 +1,4 @@
+#include <bit>
 
 #include "widgets/imagebox/imagebox.h"
 
@@ -30,44 +31,100 @@ void ImageBox::SetImage(std::shared_ptr<Image> newImage)
 	}
 }
 
-void ImageBox::SetImageMode(ImageBoxMode newMode)
+void ImageBox::SetImageScale(ImageBoxScale newScale)
 {
-	if (mode != newMode)
+	if (scale != newScale)
 	{
-		mode = newMode;
+		scale = newScale;
 		Update();
 	}
 }
 
+void ImageBox::SetImageAnchor(ImageBoxAnchor newAnchor)
+{
+	if (anchor != newAnchor)
+	{
+		anchor = newAnchor;
+		Update();
+	}
+}
+
+template <typename T>
+concept Bits = std::unsigned_integral<T> || std::is_enum_v<T>;
+
+template <Bits T, typename U = T>
+[[nodiscard]] inline constexpr U onebit(T n)
+{
+	return static_cast<U>(std::has_single_bit(n) ? n: static_cast<T>(0));
+}
+
+template <Bits T, typename U = T, typename V = T>
+[[nodiscard]] inline constexpr U only(T a, V b)
+{
+	return static_cast<U>(!(a&~b)? a: static_cast<T>(0));
+}
+
+template <Bits T, typename U = T, typename V = T>
+[[nodiscard]] inline constexpr U all(T a, V b)
+{
+	return static_cast<U>((a&b)==b? a: static_cast<T>(0));
+}
+
 void ImageBox::OnPaint(Canvas* canvas)
 {
-	if (image)
+	if (!image) return;
+
+	double bw = GetWidth(), bh = GetHeight();
+	double iw = image->GetWidth(), ih = image->GetHeight();
+	double rw = bw / iw, rh = bh / ih;
+	double xscale = 1, yscale = 1;
+
+	if (only(scale, ImageBoxScale::Contain|ImageBoxScale::Cover|ImageBoxScale::Grow|ImageBoxScale::Shrink)
+		&& (only(scale, ImageBoxScale::Contain|ImageBoxScale::Cover)
+			|| all(scale, ImageBoxScale::Grow)
+			|| all(scale, ImageBoxScale::Shrink)
+		))
 	{
-		if (mode == ImageBoxMode::Center)
+		auto rc = onebit<unsigned, ImageBoxScale>(scale & (ImageBoxScale::Cover | ImageBoxScale::Contain));
+		double rm = rc == ImageBoxScale::Cover? std::max(rw, rh): std::min(rw, rh);
+		xscale = yscale = (scale & ImageBoxScale::Grow)
+			? std::max(1., rm)
+			: (scale & ImageBoxScale::Shrink)
+				? std::min(1., rm)
+				: rm;
+	}
+	else
+	{
+		switch (onebit<unsigned, ImageBoxScale>(scale & (ImageBoxScale::StretchX | ImageBoxScale::GrowX | ImageBoxScale::ShrinkX)))
 		{
-			canvas->drawImage(image, Point((GetWidth() - (double)image->GetWidth()) * 0.5, (GetHeight() - (double)image->GetHeight()) * 0.5));
+			default: break;
+			case ImageBoxScale::StretchX: xscale = rw;            break;
+			case ImageBoxScale::GrowX:    xscale = rw > 1? rw: 1; break;
+			case ImageBoxScale::ShrinkX:  xscale = rw < 1? rw: 1; break;
 		}
-		else if (mode == ImageBoxMode::Contain)
+		switch (onebit<unsigned, ImageBoxScale>(scale & (ImageBoxScale::StretchY | ImageBoxScale::GrowY | ImageBoxScale::ShrinkY)))
 		{
-			double bw = GetWidth();
-			double bh = GetHeight();
-			double iw = image->GetWidth();
-			double ih = image->GetHeight();
-			double xscale = bw / iw;
-			double yscale = bh / ih;
-			double scale = std::min(xscale, yscale);
-			canvas->drawImage(image, Rect::xywh((bw - iw * scale) * 0.5, (bh - ih * scale) * 0.5, iw * scale, ih * scale));
-		}
-		else if (mode == ImageBoxMode::Cover)
-		{
-			double bw = GetWidth();
-			double bh = GetHeight();
-			double iw = image->GetWidth();
-			double ih = image->GetHeight();
-			double xscale = bw / iw;
-			double yscale = bh / ih;
-			double scale = std::max(xscale, yscale);
-			canvas->drawImage(image, Rect::xywh((bw - iw * scale) * 0.5, (bh - ih * scale) * 0.5, iw * scale, ih * scale));
+			default: break;
+			case ImageBoxScale::StretchY: yscale = rh;            break;
+			case ImageBoxScale::GrowY:    yscale = rh > 1? rh: 1; break;
+			case ImageBoxScale::ShrinkY:  yscale = rh < 1? rh: 1; break;
 		}
 	}
+	iw *= xscale;
+	ih *= yscale;
+
+	constexpr auto Horizontal = ImageBoxAnchor::East & ImageBoxAnchor::West;
+	constexpr auto Vertical = ImageBoxAnchor::North & ImageBoxAnchor::South;
+	double x = (anchor & Horizontal)
+		? (anchor & ~Horizontal & ImageBoxAnchor::West)
+			? 0
+			: bw-iw
+		: (bw-iw)*0.5;
+	double y = (anchor & Vertical)
+		? (anchor & ~Vertical & ImageBoxAnchor::South)
+			? bh-ih
+			: 0
+		: (bh-ih)*0.5;
+
+	canvas->drawImage(image, Rect::xywh(x, y, iw, ih));
 }
