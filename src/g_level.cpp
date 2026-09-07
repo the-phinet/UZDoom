964c4f2a121fe6e4d6e72385517d46f198204f85
Ricardo Luís Vaz Silva <ricolvs123@gmail.com>
2026-09-07 12:50:38 -0300

    cache fname conversion of map lump name
    (preparation for global shaders)

--- src/g_level.cpp
+++ src/g_level.cpp
@@ -1389,6 +1389,7 @@ void G_DoLoadLevel(const FString &nextmapname, int position, bool autosave, bool
 void FLevelLocals::DoLoadLevel(const FString &nextmapname, int position, bool autosave, bool newGame)
 {
 	MapName = nextmapname;
+	MapFName = MapName;
 	unsigned int i;
 
 	if (NextSkill >= 0)
